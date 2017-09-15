49b90384bbf21d4c44729c2a08e9be3359cf0796
Govind <shremish@indiana.edu>
2017-09-15 03:25:15 -0400

    First Commit

---
+++ src/main/java/com/project1/firstspringbootproject/FirstSpringbootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.project1.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/project1/firstspringbootproject/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.project1.firstspringbootproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class LoginController {
+	@RequestMapping("/login")
+	public String loginMessage(@RequestParam String name, ModelMap model) {
+		model.put("e" , name); 
+		return "login";
+	}	
+}
+  
